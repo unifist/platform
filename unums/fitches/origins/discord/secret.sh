19aef7073c14e39dfbbfcdccedfde50e72bb668a
Gaffer Fitch <gaffer.fitch@gmail.com>
2025-06-26 10:24:50 -0400

    Update discord

--- unums/fitches/origins/discord/secret.sh
+++ unums/fitches/origins/discord/secret.sh
@@ -6,4 +6,4 @@ NAMESPACE=$2
 read -p "Enter token: " TOKEN
 read -p "Enter guild id: " GUILD_ID
 
-kubectl --context $CLUSTER -n $NAMESPACE create secret generic "discord.json" --from-literal=data.json="{\"token\":\"$TOKEN\", \"guild\": {\"id\": \"$GUILD_ID\"}}"
+kubectl --context $CLUSTER -n $NAMESPACE create secret generic secret --from-literal=data.json="{\"token\":\"$TOKEN\", \"guild\": {\"id\": \"$GUILD_ID\"}}"
